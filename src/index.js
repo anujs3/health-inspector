90423e58c4d0ddf88cda8cdbdce80a0a26ae545c
Anuj Shah <Anuj Shah>
2017-10-02 20:58:22 -0700

    Added some hello world like code.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+    return (
+        <div>
+            <h1>Now I Do What I Want</h1>
+            <h2>By Anuj Shah</h2>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
