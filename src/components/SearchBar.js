cf19d81415356d6e24237dc3234e73854b5eb78d
Anuj Shah <Anuj Shah>
2017-10-10 15:00:26 -0700

    Made a beautiful component that can fetch data.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import 'font-awesome/css/font-awesome.css'
+import './SearchBar.css'
+import PropTypes from 'prop-types'
+
+class SearchBar extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+
+        this.state = 
+        {
+            search: ''
+        }
+    }
+
+    handleSearchChange = (e) =>
+    {
+        this.setState(
+        {
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) =>
+    {
+        e.preventDefault()
+        this.props.updateSearchTerm(this.state.search)
+    }
+
+    render()
+    {
+        return (
+            <div className="searchbar-container">
+                <form className="search-form">
+                    <input 
+                        type="search"
+                        name="search"
+                        value={this.state.search}
+                        className="search-input"
+                        placeholder="Search Restaurants"
+                        onChange={this.handleSearchChange}
+                    />
+                    <button
+                        type="submit"
+                        className="search-button"
+                        onClick={this.handleSubmit}
+                    >
+                        <i className="search-icon fa fa-search"/>
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+PropTypes.PropTypes =
+{
+    updateSearchTerm: PropTypes.func.isRequired
+}
+
+export default SearchBar
--- src/index.js
+++ src/index.js
@@ -2,17 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/SearchBar'
 
 const App = () => 
-{
+{   
+    this.updateSearchTerm = (searchTerm) => 
+    {
+        console.log(`We are searching for ${searchTerm}.`)
+        this.getSearchData(searchTerm).then((data) => 
+        {
+            console.log(data)
+        })
+    }
+
+    this.getSearchData = async (searchTerm) =>
+    {
+        let response
+        try 
+        {
+            response = await fetch(`https://data.cityofchicago.org/resource/cwig-ma7x.json?$query=SELECT * where Contains(upper(dba_name), upper("${searchTerm}")) or Contains(upper(aka_name), upper("${searchTerm}"))`)  
+        } 
+        catch (error) 
+        {
+            console.log(error)
+        }
+        let data
+        try 
+        {
+            data = await response.json()
+        }
+        catch (error) 
+        {
+            console.log(error)
+        }
+        return data
+    }
+
     return (
         <div>
-            <h1>Now I Do What I Want</h1>
-            <h2>By Anuj Shah</h2>
+            <SearchBar 
+                updateSearchTerm={this.updateSearchTerm}
+            />
         </div>
     )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-
 registerServiceWorker();
